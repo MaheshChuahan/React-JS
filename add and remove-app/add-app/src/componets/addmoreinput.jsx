f3d5907e4cd66c72b598755f43698dba0df07d63
MaheshChauhan <mahesh12041156@gmail.com>
2025-03-24 11:25:29 +0530

    24/03/2025

---
+++ add and remove-app/add-app/src/componets/addmoreinput.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+ function addmoreinput() {
+  return (
+    <React.Fragment>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <h5 className='mt-3 mb-5'>
+                        {" "}
+                        Add Remove Multiple User Input Field{" "}
+                    </h5>
+                </div>
+            </div>
+            </div>    
+  )
+}
+export default addmoreinput;
